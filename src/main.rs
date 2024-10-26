d46aac200d0513e4882e88dbd177d0ae4ceab3e5
chrxn1c <rivalofficialbusiness@outlook.com>
2024-10-27 02:53:21 +0300

    passing by

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    let file_path = "./books/frankenstein";
+    let file = std::fs::read_to_string(file_path).expect("file not found");
+    println!("contents:\n{file}");
 }
