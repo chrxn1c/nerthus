c188fc727d8262b783523ed61efe94e24f312618
chrxn1c <rivalofficialbusiness@outlook.com>
2024-10-27 04:45:25 +0300

    fixed nonascii chars

--- src/counting/mod.rs
+++ src/counting/mod.rs
@@ -1,12 +1,18 @@
 use std::collections::HashMap;
 
 pub(super) fn count_words(string: &str) -> usize {
-    string.split(" ").count()
+    string.split_ascii_whitespace().count()
 }
 
-pub(super) fn count_characters(string: &str, word_count: usize) -> HashMap<char, i64> {
+pub(super) fn count_characters(source_text: &str, word_count: usize) -> HashMap<char, i64> {
     let mut characters_to_count: HashMap<char, i64> = HashMap::with_capacity(word_count);
-    for character in string.chars() {
+    let modified_text: String = source_text
+        .chars()
+        .filter(|char| char.is_ascii_alphabetic())
+        .map(|char| char.to_owned())
+        .collect();
+
+    for character in modified_text.chars() {
         characters_to_count
             .entry(character)
             .and_modify(|counter| *counter += 1)
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod counting;
+mod preprocessing;
 mod reporting;
 
 fn main() {
     let file_path = "./books/frankenstein";
     let file = std::fs::read_to_string(file_path).expect("file not found");
-    let file = file.as_str();
 
-    let word_count = counting::count_words(file);
-    let char_count = counting::count_characters(file, word_count);
+    let word_count = counting::count_words(file.as_str());
+    let char_count = counting::count_characters(file.as_str(), word_count);
 
     let word_report = reporting::get_word_report(word_count);
     let char_report = reporting::get_chars_report(char_count);
