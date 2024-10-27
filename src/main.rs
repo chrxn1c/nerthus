f0456e7830953ebd2178d6aea6f300eecb473f36
chrxn1c <rivalofficialbusiness@outlook.com>
2024-10-27 03:21:11 +0300

    word_count + char_count

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+mod reporting;
 fn main() {
     let file_path = "./books/frankenstein";
     let file = std::fs::read_to_string(file_path).expect("file not found");
-    println!("contents:\n{file}");
+    let file = file.as_str();
+
+    let word_count = reporting::count_words(file);
+    let char_count = reporting::count_characters(file, word_count);
+
+    println!("word count: {word_count}");
+    println!("char count: {char_count:?}");
 }
